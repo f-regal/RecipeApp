5af1da742eef3ecbb43910f000019babb81a2467
Faysal Regal <faysalregal@gmail.com>
2022-08-28 21:56:28 +0100

    displayed recipe details

--- src/app/Recipe Book/Recipe List/recipe-list.component.ts
+++ src/app/Recipe Book/Recipe List/recipe-list.component.ts
@@ -9,5 +9,6 @@ import { Recipe } from '../recipe.model';
 export class RecipeList {
   title = 'RecipeList';
   recipes: Recipe[] = [ 
-    new Recipe('A Test Recipe', 'This is simply a Test', 'https://www.simplyrecipes.com/thmb/JWjdE8YwikAae0KZuyy6ZJW7Utw=/3000x2001/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1c-c2b1885d27d4481c9cfe6f6286a64342.jpg') ]
+    new Recipe('A Test Recipe', 'This is simply a Test', '../../app/Images/Recipe-logo.jpeg') 
+  ]
 }
