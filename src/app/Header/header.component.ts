e3fa320f01dbe086f41efb7e7e218f45327f20cf
Faysal Regal <faysalregal@gmail.com>
2022-09-26 19:41:49 +0100

    added navigation between recipe and shopping

--- src/app/Header/header.component.ts
+++ src/app/Header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
     selector:'app-header',
@@ -7,5 +7,18 @@ import { Component } from "@angular/core";
     
 })
 export class HeaderComponent {
+@Output() navigationChosen = new EventEmitter<{navigateTo: string}>();
 
+
+onClickRecipes() {
+    this.navigationChosen.emit({
+        navigateTo: "Recipes"
+    })
+}
+
+onClickShoppingList() {
+    this.navigationChosen.emit({
+        navigateTo: "ShoppingList"
+    })
+}
 }
--- src/app/Header/header.module.ts
+++ src/app/Header/header.module.ts
@@ -5,7 +5,6 @@ import { HeaderComponent } from './header.component';
 
 @NgModule({
   declarations: [
-    HeaderComponent
   ],
   imports: [
     BrowserModule,
